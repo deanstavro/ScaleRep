4743191f03baa6b9796fa052ab076018fe4be98e
Dean Stavropoulos <deanstavro@gmail.com>
2018-01-23 20:56:58 -0800

    change creative to homepage, simplify homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root to: 'creatives#index' #'visitors#index'
+  root to: 'homepage#index' #'visitors#index'
   devise_for :users
   resources :users
 
