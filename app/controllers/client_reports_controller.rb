90b8f71500ebaba7ef602b32703017be87e65a70
Dean Stavropoulos <deanstavro@gmail.com>
2018-03-11 16:01:48 -0700

    made reporting

--- app/controllers/client_reports_controller.rb
+++ app/controllers/client_reports_controller.rb
@@ -1,4 +1,11 @@
 class ClientReportsController < ApplicationController
   def index
+  	@user = User.find(current_user.id)
+  	puts @user
+  	@company = ClientCompany.find_by(id: @user.client_company_id)
+  	puts @company
+  	#@user = User.find_by(:id)
+  	@reports = ClientReport.where("client_company_id =?" , @company)
+
   end
 end
