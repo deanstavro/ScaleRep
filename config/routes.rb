54606e65a413ade37d01ccd18ef8741fa8e48956
Paul Stavropoulos <paulstavropoulos@gmail.com>
2018-03-11 16:32:14 -0700

    first pass at account css changes
    hi.

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  
+
   root to: 'homepage#index' #'visitors#index'
-  
-  #get '/loreta', to: 'localmarket#index'
-  get '/agent', to: 'homepage#agent'
+
+  # get '/loreta', to: 'localmarket#index'
+  # get '/agent', to: 'homepage#agent'
   devise_for :users
   resources :users
 
