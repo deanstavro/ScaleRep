0a4e975c485cde9be9d3efa0c06143134f2787a5
Dean Stavropoulos <deanstavro@gmail.com>
2018-04-01 21:57:27 -0700

    change for master

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -2,7 +2,6 @@ class Lead < ApplicationRecord
 	belongs_to :client_company, optional: false
 	belongs_to :campaign, optional: true
 
-	enum contract_sent: [:yes, :no]
 	after_initialize :init
 
 	validates :potential_deal_size, presence: true
