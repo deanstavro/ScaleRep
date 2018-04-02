8eb989a72de6b7167287d1943a8857119eea8d6c
Dean Stavropoulos <deanstavro@gmail.com>
2018-04-01 22:41:53 -0700

    try to fix RAM part 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,79 +9,84 @@ class UsersController < ApplicationController
   def show
 
     @user = User.find(params[:id])
-    @company = ClientCompany.find_by(id: @user.client_company_id)
-    @leads = Lead.where("client_company_id =? " , @company)
+    @company = @user.client_company_id
     
+    @leads = Lead.where("client_company_id =? " , @company)
     @ordered_leads = @leads.sort_by &:date_sourced
-    @leads_count = @leads.count
+    
 
-    @contracts = @leads.where(contract_amount: 1..Float::INFINITY)
-    @contracts_given = @contracts.sort_by &:date_sourced
-    @contracts_count = @contracts_given.count
-    @unconverted = @leads.count - @contracts_given.count
+    #@leads_count = @leads.count
+    #@contracts = @leads.where(contract_amount: 1..Float::INFINITY).sort_by &:date_sourced
+    #@contracts_given = @contracts.sort_by &:date_sourced
+    #@contracts_count = @contracts_given.count
+    #@unconverted = @leads.count - @contracts_given.count
 
 
     
     #Create line chart value for aggregated proposed deal sizes
-    @lead_proposed_dictionary = {}
+    lead_proposed_dictionary = {}
     @ordered_leads.each do |lead|
 
       #key, value of all leads and deals
 
-      if @lead_proposed_dictionary.has_key?(lead[:date_sourced])
-        @lead_proposed_dictionary[lead[:date_sourced]] += lead[:potential_deal_size]        
+      if lead_proposed_dictionary.has_key?(lead[:date_sourced])
+        lead_proposed_dictionary[lead[:date_sourced]] += lead[:potential_deal_size]        
 
       else
-        @lead_proposed_dictionary[lead[:date_sourced]] = lead[:potential_deal_size]
+        lead_proposed_dictionary[lead[:date_sourced]] = lead[:potential_deal_size]
 
       end
 
     end
 
+    
 
-    if @lead_proposed_dictionary.count > 1
+
+    if lead_proposed_dictionary.count > 1
       total_qua_rev = 0
     
 
-      @lead_proposed_dictionary.each { |key, value|
+      lead_proposed_dictionary.each { |key, value|
 
         total_qua_rev += value
         puts "#{key} #{value}"
-        @lead_proposed_dictionary[key] = total_qua_rev
+        lead_proposed_dictionary[key] = total_qua_rev
         }
     end
 
+    @lead_proposed_dictionary = lead_proposed_dictionary
+
 
     #Create line chart value for aggregated contract deal sizes
-    @lead_contract_dictionary = {}
+    lead_contract_dictionary = {}
     @ordered_leads.each do |lead|
 
 
       #key, value of all leads and deals
 
-      if @lead_contract_dictionary.has_key?(lead[:date_sourced])
-        @lead_contract_dictionary[lead[:date_sourced]] += lead[:contract_amount]        
+      if lead_contract_dictionary.has_key?(lead[:date_sourced])
+        lead_contract_dictionary[lead[:date_sourced]] += lead[:contract_amount]        
 
 
       else
-      @lead_contract_dictionary[lead[:date_sourced]] = lead[:contract_amount]
+      lead_contract_dictionary[lead[:date_sourced]] = lead[:contract_amount]
 
       end
 
     end
 
 
-    if @lead_contract_dictionary.count > 1
+    if lead_contract_dictionary.count > 1
       total_qua_rev = 0
-      @lead_contract_dictionary.each { |key, value|
+      lead_contract_dictionary.each { |key, value|
 
         total_qua_rev += value
         puts "#{key} #{value}"
-        @lead_contract_dictionary[key] = total_qua_rev
+        lead_contract_dictionary[key] = total_qua_rev
         }
     end
 
-
+    @lead_contract_dictionary = lead_contract_dictionary
 
 
 
