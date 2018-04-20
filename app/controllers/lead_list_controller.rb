fad968e05d61aa92e96fb2a411321962d238ded2
Paul Stavropoulos <paulstavropoulos@gmail.com>
2018-04-20 12:31:24 +0200

    Backend work to connect to air table
    done so that we can pull data straight from air table

--- app/controllers/lead_list_controller.rb
+++ app/controllers/lead_list_controller.rb
@@ -9,8 +9,23 @@ class LeadListController < ApplicationController
     # grab reports and grab leads for every week for a report
     @leads = Lead.where("client_company_id =?", @company).order('date_sourced DESC')
 
+
+    #airrecord work
+    #authenticate
+    Airrecord.api_key = "keyvWvtDSd5MDiIBZ"
+    #grab view
+    @discover_needs_opps= Opportunity.all(filter: '{lead_status} = "discover_needs"')
+    @qualified_opps= Opportunity.all(filter: '{lead_status} = "qualified"')
+
   end
 
   def edit
   end
+end
+
+
+# create Opportunity class
+class Opportunity < Airrecord::Table
+  self.base_key = "appBop3EYUrma7W4E"
+  self.table_name = "MOFU"
 end
