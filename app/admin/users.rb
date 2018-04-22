2bd66b5094d8f7646e8b7c56f442e98c9bf51b02
Dean Stavropoulos <deanstavro@gmail.com>
2018-04-22 16:34:36 -0700

    new users

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -5,7 +5,40 @@ ActiveAdmin.register User do
 # permit_params :list, :of, :attributes, :on, :model
 #
 # or
-#
+permit_params :client_company_id, :email, :encrypted_password, :first_name, :last_name, :role, :password, :password_confirmation
+
+
+
+form do |f|                         
+    f.inputs "User Details" do       
+    f.input :first_name
+    f.input :last_name
+    f.input :email
+    f.input :role
+    f.input :client_company
+    f.input :password
+    f.input :password_confirmation
+    
+    
+
+
+  end                               
+  f.actions                         
+end
+
+def update
+   @user = User.find(params[:id])
+   if params[:user][:encrypted_password].blank?
+     @user.update_without_password(params[:user])
+   else
+     @user.update_attributes(params[:user])
+   end
+   if @user.errors.blank?
+     redirect_to admin_users_path, :notice => "User updated successfully."
+   else
+     render :edit
+   end
+ end
 # permit_params do
 #   permitted = [:permitted, :attributes]
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
