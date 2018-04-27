9f5028a53467d7aee7f6b534418f581edd943592
Dean Stavropoulos <deanstavro@gmail.com>
2018-04-27 15:19:45 -0700

    fiz metric bug

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -7,7 +7,7 @@ before_action :authenticate_user!
 
     @company = @user.client_company_id
     
-    @leads = Lead.where("client_company_id =? " , @company)
+    @leads = Lead.where(:client_company_id => @company, :meeting_set => true)
     @ordered_leads = @leads.sort_by &:date_sourced
     
 
@@ -21,19 +21,27 @@ before_action :authenticate_user!
     
     #Create line chart value for aggregated proposed deal sizes
     lead_proposed_dictionary = {}
-    @ordered_leads.each do |lead|
 
-      #key, value of all leads and deals
+    if @ordered_leads.size < 1
+      puts "no leads"
+    else
 
-      if lead_proposed_dictionary.has_key?(lead[:date_sourced])
-        lead_proposed_dictionary[lead[:date_sourced]] += lead[:potential_deal_size]        
+      @ordered_leads.each do |lead|
 
-      else
-        lead_proposed_dictionary[lead[:date_sourced]] = lead[:potential_deal_size]
+        #key, value of all leads and deals
+
+        if lead_proposed_dictionary.has_key?(lead[:date_sourced])
+          lead_proposed_dictionary[lead[:date_sourced]] += lead[:potential_deal_size]        
+
+        else
+          lead_proposed_dictionary[lead[:date_sourced]] = lead[:potential_deal_size]
+
+        end
 
       end
-
     end
+
+
 
   end
 
