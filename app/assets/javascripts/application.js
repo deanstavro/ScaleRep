5cf3b0a0d327807a008b70ad67925045b8f0101b
lucasocon <lucas_masterclas@hotmail.com>
2018-05-15 18:23:36 -0300

    Tooltip on Add Leads, Fix on Persona.name for Campaign

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,12 @@
 //= require bootstrap
 //= require Chart.bundle
 //= require chartkick
+
+
+$( document ).on('turbolinks:load', function() {
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip({
+      template: '<div class="tooltip"><div class="tooltip-arrow"></div><div class="tooltip-head"><dl><dt>Upload Requirements:</dt><dd>- Uploaded file must be a csv</dd><dd>- Uploaded csv must contain header fields first_name, last_name, email, and company</dd><dt>CSV fields:</dt><dd>- first_name</dd><dd>- last_name</dd><dd>- email</dd><dd>- phone_type</dd><dd>- phone_number</dd><dd>- title</dd><dd>- linkedin</dd><dd>- timezone</dd><dd>- company</dd><dd>- company_domain</dd><dd>- address</dd><dd>- city</dd><dd>- state</dd><dd>- country</dd><dd>- company_description</dd><dd>- number_of_employees</dd><dd>- last_funding_type</dd><dd>- last_funding_date</dd><dd>- last_funding_amount</dd><dd>- total_funding_amount</dd><dd>- email_snippet</dd></dl></div></div>'
+    });
+  });
+});
