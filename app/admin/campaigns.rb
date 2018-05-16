a07e9c011fd16a8ca7cea065e0b4e1fbdd88fc36
Dean Stavropoulos <deanstavro@gmail.com>
2018-05-16 10:37:47 -0700

    fix active admin persona

--- app/admin/campaigns.rb
+++ app/admin/campaigns.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Campaign do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :reply_id, :reply_key, :persona, :user_notes, :industry, :campaign_start, :campaign_end, :client_company_id, :name, :personalized
+permit_params :reply_id, :reply_key, :persona, :user_notes, :industry, :campaign_start, :campaign_end, :client_company_id, :name, :personalized, :persona_id
 # or
 #
 # permit_params do
