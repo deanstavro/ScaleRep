9aef92c13fbbfed8d9d22a87b98d8256736fa52d
Dean Stavropoulos <deanstavro@gmail.com>
2018-05-16 13:56:46 -0700

    fix bugs to account for bad reply keys

--- app/jobs/get_campaigns_from_reply_job.rb
+++ app/jobs/get_campaigns_from_reply_job.rb
@@ -7,48 +7,55 @@ class GetCampaignsFromReplyJob < ApplicationJob
 
     # Loop through all companies
     ClientCompany.find_each do |company|
-      company.campaigns.find_each do |campaign|
-        puts campaign.persona
 
-        if campaign.reply_id.to_i > 0
-          if campaign.reply_key.length > 0
-            puts "CAMPAIGN CREDENTIALS"
-            puts campaign.reply_id
-            puts campaign.reply_key
+      if company.replyio_keys.length>0
+          company.campaigns.find_each do |campaign|
+            puts campaign.persona
 
-            response = v1_get_campaign(campaign.reply_id.to_i, campaign.reply_key)
+            if campaign.reply_id.to_i > 0
+              if campaign.reply_key.length > 0
+                begin
+                    puts "CAMPAIGN CREDENTIALS"
+                    puts campaign.reply_id
+                    puts campaign.reply_key
+
+                    response = v1_get_campaign(campaign.reply_id.to_i, campaign.reply_key)
+                    
+                    response[:last_poll_from_reply] = response[:created]
+                    response[:campaign_name] = response[:name]
+                    response.delete(:name)
+                    response.delete(:id)
+                    response.delete(:created)
+                    puts response
+
+                    
+                    
+                    campaign.update_attributes(response)
+
+                    puts campaign.last_poll_from_reply
+
+
+                    # Update the campaign in the local database
+
+
+
+                    sleep 60
+                rescue
+                    puts "COULD NOT PULL METRICS FOR CAMPAIGN " + campaign.campaign_name
+                end
+              end
+            end
+
+                #call correct API to retrieve metrics
+
+
+            # Get the response
+
+
+            # For each metric
+
             
-            response[:last_poll_from_reply] = response[:created]
-            response[:campaign_name] = response[:name]
-            response.delete(:name)
-            response.delete(:id)
-            response.delete(:created)
-            puts response
-
-            
-            
-            campaign.update_attributes(response)
-
-            puts campaign.last_poll_from_reply
-
-
-            # Update the campaign in the local database
-
-
-
-            sleep 60
-          end
         end
-
-            #call correct API to retrieve metrics
-
-
-        # Get the response
-
-
-        # For each metric
-
-        
       end
     end
 
