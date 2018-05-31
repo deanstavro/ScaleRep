8241d0d91cf728d8292acbf9c299b0a56a1e0915
Dean Stavropoulos <deanstavro@gmail.com>
2018-05-31 15:38:09 -0700

    take out extra nav

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,19 +4,20 @@ class HomepageController < ApplicationController
   def index
 
   	#ahoy.track_visit
-    puts "YOOOO"
+
   	if user_signed_in?
   		@user = current_user
       if @user.client_company.account_live == true
-        puts "YES"
+        puts "Account Live - Direct to Persona Page"
   		  redirect_to client_company_campaigns_path(@user.client_company)
       else
         redirect_to user_path(@user)
-        puts "NO"
+        puts "Account Not Live - Direct to Onboarding"
 
       end
     else
-      @demo = Demo.new
+        @demo = Demo.new
+        puts "User not signed in - direct to home"
   	end
   end
 
