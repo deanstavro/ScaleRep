ebe95d0c107fb25e77cd81807c502e309151aa6d
Dean Stavropoulos <deanstavro@gmail.com>
2018-09-16 18:11:24 -0700

    fix cleaning bug

--- app/jobs/clean_upload_job.rb
+++ app/jobs/clean_upload_job.rb
@@ -22,37 +22,48 @@ class CleanUploadJob < ApplicationJob
 	    all_hash = []
 	    duplicates = []
 	    not_imported = []
-	    puts "we got her actualy"
 
+	    puts "we got here"
 	    data_copy.each_with_index do |contact, index|
 			delete_row = false
+			puts "and here"
 
 			if contact["email"].present? and contact["first_name"].present?
-				puts "yup"
+
 				# Check if admin wants to ignore duplicates
 				if data_upload.ignore_duplicates == false
 					puts "herro"
 				  	# check for duplicates
 				  	if client_leads.where(:email => contact["email"]).count == 0
+				  		puts "not a duplicate"
 				      	default = data_upload.rules.to_s
-				      	if default == DataUpload.columns_hash["rules"].default
+
+				      	puts "check wat rules equal"
+				      	if data_upload.rules.empty?
+				      		puts "empty rules"
 				          	# Insert the hash into the main hash
 				          	all_hash << contact.to_h
 				      	else
 
-					        # Loop through rules and apply to row. if we need to delete row, break loop and delete!
-					        rules_array = data_upload.rules
+				      		begin
+				      			puts "defaul different, going through rules"
+					        	# Loop through rules and apply to row. if we need to delete row, break loop and delete!
+					        	rules_array = data_upload.rules
 					        
-					        rules_array.each_with_index do |rule, index|
-				          		delete_row, contact =  apply_rule(rule, contact)
-				          		if delete_row == true
-				            		break
-				          		end
-				        	end
+						        rules_array.each_with_index do |rule, index|
+					          		delete_row, contact =  apply_rule(rule, contact)
+					          		if delete_row == true
+					            		break
+					          		end
+					        	end
 
-				        	if !delete_row
-				          		all_hash << contact.to_h
-				        	end
+					        	if !delete_row
+					          		all_hash << contact.to_h
+					        	end
+					        rescue
+					        	puts "could not go through data cleaning"
+					        	all_hash << contact.to_h
+					        end
 				      	end
 			  		else
 			    		duplicates << contact
@@ -88,11 +99,74 @@ class CleanUploadJob < ApplicationJob
 				not_imported << contact
 			end
 		end
-
-		data_upload.update_attributes(:cleaned_data => all_hash, :duplicates => duplicates, :not_imported => not_imported)
 		puts "ALL HASH " + all_hash.to_s
+		data_upload.update_attributes(:cleaned_data => all_hash, :duplicates => duplicates, :not_imported => not_imported)
+		
 
   	end
+
+  	private
+
+
+	#Apply rule to contact
+	def apply_rule(rule, contact)
+		puts "cleaning " + contact["email"] + " with rule " + rule
+
+		begin
+
+		  if rule.include? "delete row"
+		    rule_column = rule.split("'")[1]
+		    puts "delete row " + rule_column + " with string " + contact[rule_column]
+
+		      if contact[rule_column].include? rule.split("'")[3]
+		          return true, contact
+		      else
+		          return false, contact
+		      end
+
+		  elsif rule.include? "delete string"
+
+		    rule_column  = rule.split("'")[3]
+		    string_delete = rule.split("'")[1]
+		    puts "delete string " + string_delete + " with string " + contact[rule_column]
+
+		    #lower case sensitive check
+		    val_cop = contact[rule_column].downcase
+		    string_del_cop = string_delete.downcase
+
+		    if val_cop.include? string_del_cop
+		      contact[rule_column] = val_cop.gsub(string_delete, '')        
+		      puts contact[rule_column]
+		    end
+
+		    
+		    return false, contact
+
+		  elsif rule.include? "change casing"
+		    rule_column  = rule.split("'")[1]
+		    check_length = rule.split("'")[3]
+		    puts "delete casing " + rule_column + "for length greater than " + check_length.to_s
+
+		    if contact[rule_column].length > check_length.to_i
+		      new_value = contact[rule_column].split.map(&:capitalize).join(' ')
+		      contact[rule_column] = new_value
+		    end
+		    
+		    return false, contact
+
+		  else 
+
+		    puts "can't understand rule"
+		    return false, contact
+
+		  end   
+
+		rescue
+		    puts "ERROR"
+		    return false, contact
+		end 
+
+	end
 
 
 end
