a9bf875c31d0afec3e10782bc5f36fe26a7ecd80
Dean Stavropoulos <deanstavro@gmail.com>
2018-10-08 19:49:43 -0700

    add replies to metrics graph

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -7,7 +7,11 @@ class MetricsController < ApplicationController
 	@company = ClientCompany.find_by(id: @user.client_company_id)
 
 	@touchpoints = Touchpoint.where(['client_company_id = ? and created_at >= ?', @company, Date.today - 7.days])
-    @lead_actions = LeadAction.where(['client_company_id = ? and first_time = ? and created_at >= ?', @company, 'True', Date.today - 7.days ])
+
+	@lead_actions = LeadAction.where(['client_company_id = ? and created_at >= ?',@company, Date.today - 7.days ])
+	@email_opens = @lead_actions.where(['action = ? and first_time = ?', LeadAction.actions[:open], 'True'])
+    @email_replies = @lead_actions.where(['action = ?', LeadAction.actions[:reply]])
+    
     @meetings_set = Lead.where(client_company_id: @company.id).where(:status => ["handed_off", "handed_off_with_questions", "sent_meeting_invite"]).order('updated_at DESC').paginate(:page => params[:page], :per_page => 20)
 
   end
