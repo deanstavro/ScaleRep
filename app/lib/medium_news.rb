752053ee41ab9d84f5a37ce370ea26c1776df923
Dean Stavropoulos <deanstavro@gmail.com>
2018-11-16 00:48:58 -0800

    v1 blog on site complete

--- app/lib/medium_news.rb
+++ app/lib/medium_news.rb
@@ -4,6 +4,7 @@ require 'open-uri'
 module Medium_News
 	def self.medium_news
 		rss = RSS::Parser.parse(open('https://medium.com/feed/@ScaleRep').read, false).items[0..5]
-		rss.map{|article| {title: article.title, date: article.pubDate, link: article.link, description: article.description, creator: article.dc_creator}}
+		rss.map{|article| 
+			{title: article.title, date: article.pubDate, link: article.link, description: article.description, creator: article.dc_creator, img: article.content_encoded.split("figure")[1].to_s.split('"')[3] }}
 	end
 end
