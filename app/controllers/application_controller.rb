71d3f43edfc2fd14895acf353b59fb93c361c6b3
deanstavro <deanstavro@gmail.com>
2018-12-19 17:27:25 -0800

    change log in scalerep link to wordpress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_out_path_for(resource_or_scope)
-    "https://www.scalerep.com"
+    "https://scalerep.com"
   end
 
 end
