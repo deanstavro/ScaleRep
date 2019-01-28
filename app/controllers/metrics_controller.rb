631b3e33d2015a1a3b6f9dffe8248a86cf2ffe80
Paul Stavropoulos <paulstavropoulos@gmail.com>
2019-01-27 21:15:30 -0800

    added chart options for week and monthly
    hi

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -4,7 +4,8 @@ class MetricsController < ApplicationController
 
   def index
     @user = User.find(current_user.id)
-  
+
+    # to display metrics by company
     if @user.role == "scalerep"
         @client_companies = ClientCompany.where("account_live = ?", true).pluck(:name)
         if params.has_key?(:client_company)
@@ -15,12 +16,19 @@ class MetricsController < ApplicationController
     else
           @company = ClientCompany.find_by(id: @user.client_company_id)
     end
-    
+
+    # what time_frame
+    if params.has_key?("time_frame")
+      @time_frame = params["time_frame"]
+    else
+      @time_frame = "Last 7 Days"
+    end
+
   	@touchpoints = Touchpoint.where(['client_company_id = ? and created_at >= ?', @company, Date.today - 7.days])
   	@lead_actions = LeadAction.where(['client_company_id = ? and email_sent_time >= ?',@company, Date.today - 7.days ])
   	@email_opens = @lead_actions.where(['action = ? and first_time = ?', LeadAction.actions[:open], 'True'])
     @email_replies = @lead_actions.where(['action = ?', LeadAction.actions[:reply]])
-    @meetings_set = Lead.where(client_company_id: @company.id).where(:status => ["handed_off", "handed_off_with_questions", "sent_meeting_invite"]).order('date_sourced DESC').paginate(:page => params[:page], :per_page => 5)
+    @meetings_set = Lead.where(client_company_id: @company.id).where(:status => ["handed_off", "handed_off_with_questions", "sent_meeting_invite"]).order('date_sourced DESC').paginate(:page => params[:page], :per_page => 10)
   end
 
 end
