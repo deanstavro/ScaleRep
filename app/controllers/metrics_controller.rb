82a7adf32642f6a17b5633b59f536c2ae023e60f
deanstavro <deanstavro@gmail.com>
2019-01-28 11:23:48 -0800

    time range fix

--- app/controllers/metrics_controller.rb
+++ app/controllers/metrics_controller.rb
@@ -20,12 +20,24 @@ class MetricsController < ApplicationController
     # what time_frame
     if params.has_key?("time_frame")
       @time_frame = params["time_frame"]
+
+      if @time_frame == "By Week"
+        @range = Date.today - 56.days
+      else
+        @range = Date.today - 240.days
+      end
+      
     else
       @time_frame = "Last 7 Days"
+      @range = Date.today - 7.days
     end
 
-  	@touchpoints = Touchpoint.where(['client_company_id = ? and created_at >= ?', @company, Date.today - 7.days])
-  	@lead_actions = LeadAction.where(['client_company_id = ? and email_sent_time >= ?',@company, Date.today - 7.days ])
+
+
+
+
+  	@touchpoints = Touchpoint.where(['client_company_id = ? and created_at >= ?', @company, @range])
+  	@lead_actions = LeadAction.where(['client_company_id = ? and email_sent_time >= ?',@company, @range ])
   	@email_opens = @lead_actions.where(['action = ? and first_time = ?', LeadAction.actions[:open], 'True'])
     @email_replies = @lead_actions.where(['action = ?', LeadAction.actions[:reply]])
     @meetings_set = Lead.where(client_company_id: @company.id).where(:status => ["handed_off", "handed_off_with_questions", "sent_meeting_invite"]).order('date_sourced DESC').paginate(:page => params[:page], :per_page => 10)
