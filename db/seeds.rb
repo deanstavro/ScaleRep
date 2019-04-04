4e79d54e0f4be69508ff4dcbd94ef3589a9a22b4
Paul Stavropoulos <paulstavropoulos@gmail.com>
2019-04-03 20:58:44 -0700

    just added to seeds -- want to do more but running into an issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,10 @@
 #user = CreateAdminService.new.call
 #puts 'CREATED ADMIN USER: ' << user.email
 
-
+#admin users
 AdminUser.create!(email: 'dean@scalerep.com', password: 'Sc@leRep123!', password_confirmation: 'Sc@leRep123!') unless AdminUser.exists?(email: 'dean@scalerep.com')
 AdminUser.create!(email: 'paul@scalerep.com', password: 'Sc@leRep123!', password_confirmation: 'Sc@leRep123!') unless AdminUser.exists?(email: 'paul@scalerep.com')
+
+# client client_companies
+papito = ClientCompany.create!(name:'Papito', company_domain:'papito.com', monthlyContactEngagementCount: 200, account_manager: 'Isaias Bustos') unless ClientCompany.exists?(company_domain: 'papito.com')
+ClientCompany.create!(name:'Sunoco', company_domain:'sunoco.com', monthlyContactEngagementCount: 200, account_manager: 'Isaias Bustos') unless ClientCompany.exists?(company_domain: 'sunoco.com')
