b9eac782cfa81eabf0e776a71e0e2f2e65c1de1f
deanstavro <deanstavro@gmail.com>
2019-04-12 16:08:22 -0700

    change account model to fix lead upload error

--- app/models/account.rb
+++ app/models/account.rb
@@ -2,7 +2,7 @@ class Account < ApplicationRecord
 	has_many :leads
 	belongs_to :client_company, optional: false
 
-	validates_uniqueness_of :website, scope: :client_company_id
+	validates_uniqueness_of :website, scope: :client_company_id, :allow_blank => true, :allow_nil => true
 	validates_uniqueness_of :name, presence: true, allow_blank: false
 
 	enum status: [:cold, :in_campaign, :blacklist, :customer]
